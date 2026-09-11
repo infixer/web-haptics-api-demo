cdbd2c83e757c493602ecae9575feaf772772d02
Ryo Katsuse <ryokatsu0719@gmail.com>
2026-09-11 18:16:37 +0900

    操作デモをトップページへ移動してlabを転送

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
   import "../app.css";
-  import { haptics } from "$lib/haptics.svelte.js";
 
   let { children } = $props();
 
-  $effect(() => {
-    haptics.start();
-  });
 </script>
 
 {@render children()}
